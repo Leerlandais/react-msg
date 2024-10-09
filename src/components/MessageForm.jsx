1c2bb3ff95a8d7b4c1c55460cac82f48d7fe0e48
leerlandais <leerlandais@gmail.com>
2024-10-09 14:28:19 +0200

    moved form to component file

---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,29 @@
+
+export default function MessageForm() {
+    return (
+        <form method="POST" className="myForm" id="myForm">
+            <h2 id="h2Message">Laissez-nous un message</h2>
+            <div className="formDiv div_firstname">
+                <label htmlFor="firstname" id="label_firstname" className="inputLabel">Prénom</label>
+                <input type="text" name="firstname" id="inputFirstName" className="inputText input_firstName"/>
+            </div>
+            <div className="formDiv div_lastname">
+                <label htmlFor="lastname" id="label_lastname" className="inputLabel">Nom</label>
+                <input type="text" name="lastname" id="inputLastName" className="inputText input_lastName"/>
+            </div>
+            <div className="formDiv div_usermail">
+                <label htmlFor="usermail" id="label_usermail" className="inputLabel">E-mail</label>
+                <input type="text" name="usermail" id="inputUserMail" className="inputText input_usermail"/>
+            </div>
+            <div className="formDiv div_message">
+                <label htmlFor="message" id="label_message" className="inputLabel label_mess">Message</label>
+                <textarea name="message" id="inputMessage" className="inputText input_message" cols="20"
+                          rows="2"></textarea>
+            </div>
+            <div className="formDiv div_subButton">
+                <button className="subButton" id="subButton">Envoyer</button>
+                <span className="lengthSpan" id="lengthSpan">0</span><span className="lengthSpan">/600</span>
+            </div>
+        </form>
+    );
+}
