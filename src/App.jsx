129f14ebbaeebed7dc1aaaa41d12fc3a84cc4230
leerlandais <leerlandais@gmail.com>
2024-10-09 13:02:34 +0200

    added basic form and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+
+          <h1>Livre d&apos;or</h1>
+          <div className="upperSection">
+              <div className="imageDiv">
+                  <img src="img/email.png" alt="emailLogo" />
+              </div>
+          <form action="" method="POST" className="myForm" id="myForm">
+              <h2 id="h2Message">Laissez-nous un message</h2>
+              <div className="formDiv div_firstname">
+                  <label htmlFor="firstname" id="label_firstname" className="inputLabel">Prénom</label>
+                  <input type="text" name="firstname" id="inputFirstName" className="inputText input_firstName"/>
+              </div>
+              <div className="formDiv div_lastname">
+                  <label htmlFor="lastname" id="label_lastname" className="inputLabel">Nom</label>
+                  <input type="text" name="lastname" id="inputLastName" className="inputText input_lastName"/>
+              </div>
+              <div className="formDiv div_usermail">
+                  <label htmlFor="usermail" id="label_usermail" className="inputLabel">E-mail</label>
+                  <input type="text" name="usermail" id="inputUserMail" className="inputText input_usermail"/>
+              </div>
+              <div className="formDiv div_message">
+                  <label htmlFor="message" id="label_message" className="inputLabel label_mess">Message</label>
+                  <textarea name="message" id="inputMessage" className="inputText input_message" cols="20"
+                            rows="2"></textarea>
+              </div>
+              <div className="formDiv div_subButton">
+                  <button className="subButton" id="subButton">Envoyer</button>
+                  <span className="lengthSpan" id="lengthSpan">0</span><span className="lengthSpan">/600</span>
+              </div>
+          </form>
+          </div>
+      </>
   )
 }
 
