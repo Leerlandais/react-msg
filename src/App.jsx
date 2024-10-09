66dcc2e6e35426fda56bd55d703303a10015e0f7
leerlandais <leerlandais@gmail.com>
2024-10-09 16:55:33 +0200

    correctly moved form into component

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,18 @@ import MessageForm from "./components/MessageForm.jsx";
 
 function App() {
 
-    const [newMsg, setNewMsg] = useState("")
+
     const [messages, setMessages] = useState([])
 
-    function formSubmission(e) {
-        e.preventDefault()
-            setMessages((currentMessages) => {
-                return [
-                    ...currentMessages,
-                    { id: crypto.randomUUID(),
-                        text: newMsg }
-                ]
-            })
-        setNewMsg("")
-    }
+function addMessage(text) {
+    setMessages((currentMessages) => {
+        return [
+            ...currentMessages,
+            { id: crypto.randomUUID(),
+                text: text }
+        ]
+    })
+}
 console.log(messages)
   return (
       <>
@@ -32,75 +30,7 @@ console.log(messages)
                   <img src="../public/images/email.png" alt="emailLogo"/>
               </div>
 
-              <form onSubmit={formSubmission}
-                    className="myForm"
-                    id="myForm">
-                  <h2 id="h2Message">
-                      Laissez-nous un message
-                  </h2>
-                  <div className="formDiv div_firstname">
-                      <label htmlFor="firstname"
-                             id="label_firstname"
-                             className="inputLabel">
-                          Prénom
-                      </label>
-                      <input type="text"
-                             name="firstname"
-                             id="inputFirstName"
-                             className="inputText input_firstName"/>
-                  </div>
-                  <div className="formDiv div_lastname">
-                      <label htmlFor="lastname"
-                             id="label_lastname"
-                             className="inputLabel">
-                          Nom
-                      </label>
-                      <input type="text"
-                             name="lastname"
-                             id="inputLastName"
-                             className="inputText input_lastName"/>
-                  </div>
-                  <div className="formDiv div_usermail">
-                      <label htmlFor="usermail"
-                             id="label_usermail"
-                             className="inputLabel">
-                          E-mail
-                      </label>
-                      <input type="text"
-                             name="usermail"
-                             id="inputUserMail"
-                             className="inputText input_usermail"/>
-                  </div>
-                  <div className="formDiv div_message">
-                      <label htmlFor="message"
-                             id="label_message"
-                             className="inputLabel label_mess">
-                          Message
-                      </label>
-                      <textarea name="message"
-                                id="inputMessage"
-                                className="inputText input_message"
-                                cols="20"
-                                rows="2"
-                                value={newMsg}
-                                onChange={(e) => setNewMsg(e.target.value)}
-                      >
-                      </textarea>
-                  </div>
-                  <div className="formDiv div_subButton">
-                      <button className="subButton"
-                              id="subButton">
-                          Envoyer
-                      </button>
-                      <span className="lengthSpan"
-                            id="lengthSpan">
-                          0
-                      </span>
-                      <span className="lengthSpan">
-                          /600
-                      </span>
-                  </div>
-              </form>
+                <MessageForm onSubmit={addMessage} />
 
           </div>
 
--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,28 +1,84 @@
+import {useState} from "react";
 
-export default function MessageForm() {
+export default function MessageForm(props) {
+
+    const [newMsg, setNewMsg] = useState("")
+
+
+    function formSubmission(e) {
+        e.preventDefault()
+        if (newMsg === "") return
+        props.onSubmit(newMsg)
+        setNewMsg("")
+    }
     return (
-        <form method="POST" className="myForm" id="myForm">
-            <h2 id="h2Message">Laissez-nous un message</h2>
+        <form onSubmit={formSubmission}
+              className="myForm"
+              id="myForm">
+            <h2 id="h2Message">
+                Laissez-nous un message
+            </h2>
             <div className="formDiv div_firstname">
-                <label htmlFor="firstname" id="label_firstname" className="inputLabel">Prénom</label>
-                <input type="text" name="firstname" id="inputFirstName" className="inputText input_firstName"/>
+                <label htmlFor="firstname"
+                       id="label_firstname"
+                       className="inputLabel">
+                    Prénom
+                </label>
+                <input type="text"
+                       name="firstname"
+                       id="inputFirstName"
+                       className="inputText input_firstName"/>
             </div>
             <div className="formDiv div_lastname">
-                <label htmlFor="lastname" id="label_lastname" className="inputLabel">Nom</label>
-                <input type="text" name="lastname" id="inputLastName" className="inputText input_lastName"/>
+                <label htmlFor="lastname"
+                       id="label_lastname"
+                       className="inputLabel">
+                    Nom
+                </label>
+                <input type="text"
+                       name="lastname"
+                       id="inputLastName"
+                       className="inputText input_lastName"/>
             </div>
             <div className="formDiv div_usermail">
-                <label htmlFor="usermail" id="label_usermail" className="inputLabel">E-mail</label>
-                <input type="text" name="usermail" id="inputUserMail" className="inputText input_usermail"/>
+                <label htmlFor="usermail"
+                       id="label_usermail"
+                       className="inputLabel">
+                    E-mail
+                </label>
+                <input type="text"
+                       name="usermail"
+                       id="inputUserMail"
+                       className="inputText input_usermail"/>
             </div>
             <div className="formDiv div_message">
-                <label htmlFor="message" id="label_message" className="inputLabel label_mess">Message</label>
-                <textarea name="message" id="inputMessage" className="inputText input_message" cols="20"
-                          rows="2"></textarea>
+                <label htmlFor="message"
+                       id="label_message"
+                       className="inputLabel label_mess">
+                    Message
+                </label>
+                <textarea name="message"
+                          id="inputMessage"
+                          className="inputText input_message"
+                          cols="20"
+                          rows="2"
+                          value={newMsg}
+                          onChange={(e) => setNewMsg(e.target.value)}
+                >
+                      </textarea>
             </div>
             <div className="formDiv div_subButton">
-                <button className="subButton" id="subButton">Envoyer</button>
-                <span className="lengthSpan" id="lengthSpan">0</span><span className="lengthSpan">/600</span>
+                <button className="subButton"
+                        id="subButton">
+                    Envoyer
+                </button>
+                <span className="lengthSpan"
+                      id="lengthSpan">
+                          0
+                      </span>
+                <span className="lengthSpan">
+                          /600
+                      </span>
             </div>
         </form>
     );
